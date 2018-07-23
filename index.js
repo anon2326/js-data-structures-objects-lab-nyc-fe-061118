54f960740cf4a96e5f9b299806e2cca1d44c8502
Jeffrey Jiang <anon0503@gmail.com>
2018-07-23 23:16:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
   driver[key] = value;
   return driver;
 }
+
+function deleteFromDriverByKey(driver, key){
+  const newDriver = {...driver};
+  delete newDriver.key;
+}

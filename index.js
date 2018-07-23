996f4d07156299e49d5237d8347284b10030c2b7
Jeffrey Jiang <anon0503@gmail.com>
2018-07-23 22:48:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
-const driver = {};
+const driver = {name: Sam; };
 function updateDriverWithKeyAndValue(driver, key, value){
-  newDriver = driver.slice();
+  newDriver.assign(driver);
 
 }
